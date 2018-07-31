119c60202e9e34a193dfbabfcfc2d59e9f4d8eef
Unknown <josephlukeedwards@gmail.com>
2018-07-31 19:59:50 +0100

    Create Hello World div

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
+// React will be the local name for the library 'react' in the node_modules
+// The 'react' library is for creating and managing components, 
+// 'react-dom' for DOM interactions
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+// Create a new component. This should produce some HTML
+// "const" is ES6 for declaring constants, "() =>" is equivalent to function()
+// This creates a component class - which we can then create instances of
+const App = () => {
+  // This is JSX - it looks like HTML but it's actually JS
+  // This also can't be understood by a browser and must be rendered by webpack
+  return <div>Hi!</div>;
+}
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// Take this component's generated HTML and put it on the page (in the DOM)
+// We can only render instances, not classes
+// <App></App> (or </App /> for short) instantiates the App class above
+// Second argument is the container - where the element should be rendered
+// In this case index.html already has a div class container, so use that
+ReactDOM.render(<App />, document.querySelector('.container'));
