c441c5371e5f6f4e219801d30dc9e632f155dde4
Unknown <josephlukeedwards@gmail.com>
2018-08-02 17:01:55 +0100

    Add constructor to SearchBar

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -6,13 +6,28 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+  // if this function exists it is called automatically 
+  // when a new instance is created
+  constructor(props) {
+    // this calls the constructor method of the parent Component class
+    super(props);
+    // initialise the state with a property for the search term
+    this.state = { term: 'Starting Value' };
+  }
+  
   // classes extending React.Component must define a render method that
   // returns some JSX
   render() {
     // all HTML elements can emit a "change" event when interacted with
     // the onChange property watches for that event on that element 
     // and calls the supplied event handler when it happens
-    return <input onChange={event => console.log(event.target.value)} />;
+    return (
+      <div>
+        <input 
+          value={this.state.term}
+          onChange={event => this.setState({ term: event.target.value })} />
+      </div>  
+    );
   }
 
   // event handler
